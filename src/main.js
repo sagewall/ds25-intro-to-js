51b48be850bcd3c907d90742371c877a3749c563
Sage Wall <swall@esri.com>
2025-02-10 09:11:02 -0700

    feat: add layer list and legend

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-import "./style.css";
+import "@arcgis/map-components/components/arcgis-layer-list";
+import "@arcgis/map-components/components/arcgis-legend";
 import "@arcgis/map-components/components/arcgis-map";
+import "./style.css";
