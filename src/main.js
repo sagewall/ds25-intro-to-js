d1f7e34da10e4b70ad361e21f7bd884bae760cc6
Sage Wall <swall@esri.com>
2025-02-18 15:16:48 -0700

    feat: add feature table

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
+import "@arcgis/map-components/components/arcgis-feature-table";
 import "@arcgis/map-components/components/arcgis-layer-list";
 import "@arcgis/map-components/components/arcgis-legend";
 import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-scene";
+import "@esri/calcite-components/components/calcite-shell";
 import "./style.css";
 
 const arcgisLayerList = document.querySelector("arcgis-layer-list");
+const arcgisFeatureTable = document.querySelector("arcgis-feature-table");
 
-arcgisLayerList.addEventListener("arcgisReady", () => {
+handleArcgisLayerListReady();
+
+async function handleArcgisLayerListReady() {
+  await arcgisLayerList.componentOnReady();
   arcgisLayerList.listItemCreatedFunction = (event) => {
     const { item } = event;
-    if (item.layer.type !== "group") {
-      item.panel = {
-        content: "legend",
-        open: true,
-      };
-    }
+
+    item.panel = {
+      content: "legend",
+      open: true,
+    };
   };
-});
+
+  reactiveUtils.watch(
+    () => arcgisLayerList.selectedItems.getItemAt(0),
+    (item) => {
+      arcgisFeatureTable.layer = item.layer;
+    }
+  );
+}
